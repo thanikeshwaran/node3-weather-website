b2913e751bae1ec56c235c49b4b3d88fdee20ab3
thanikeshwaran <thanikeshwaran132@gmail.com>
2022-07-01 06:16:36 -0600

    changed port method in app.js src file

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ console.log(path.join(__dirname, "../public"));
 
 const app = express(); // express is a function (not an object)
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, "../public");
